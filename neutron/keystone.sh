c69be1209daf41b163ef7ef0dd78f0ed5e768455
Dan Prince <dprince@redhat.com>
2020-01-10 09:55:34 -0500

    neutron: Update internal endpoint

--- neutron/keystone.sh
+++ neutron/keystone.sh
@@ -2,5 +2,5 @@ openstack user create --domain default --password CAspTtEy7vjtzbpvPLJThLJ4b neut
 openstack role add --project service --user neutron admin
 openstack service create --name neutron --description "OpenStack Network" network
 openstack endpoint create --region RegionOne network public http://openstack-neutron-test.apps.test.dprince/
-openstack endpoint create --region RegionOne network internal http://openstack-neutron-test.apps.test.dprince/
 openstack endpoint create --region RegionOne network admin http://openstack-neutron-test.apps.test.dprince/
+openstack endpoint create --region RegionOne network internal http://openstack-neutron/
